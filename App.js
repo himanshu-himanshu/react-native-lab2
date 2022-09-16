a37b8b2d07f682c650a3b0e118b777423eb89c45
Himanshu <himanshu16315@gmail.com>
2022-09-15 21:01:25 -0400

    Toggle button for changing theme to dark (Task 3)

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
-  Button,
   TouchableOpacity,
+  Switch,
 } from "react-native";
 
 export default function App() {
   const [text, setText] = React.useState(" ");
+  const [dark, setDark] = React.useState(false);
+  const [isEnabled, setIsEnabled] = React.useState(false);
+
+  const toggleSwitch = () => {
+    setIsEnabled(!isEnabled);
+    setDark(!dark);
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, dark ? styles.dark : ""]}>
       <Text style={styles.title}>React Native Lab 2</Text>
       <TextInput
         style={styles.inputField}
@@ -27,6 +34,15 @@ export default function App() {
       >
         <Text style={styles.clearButton}>Clear Text</Text>
       </TouchableOpacity>
+      <Switch
+        style={styles.switch}
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={() => toggleSwitch()}
+        value={isEnabled}
+      ></Switch>
+      <Text style={styles.author}>Submitted By: Himanshu (301296001)</Text>
     </View>
   );
 }
@@ -37,6 +53,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  dark: {
+    backgroundColor: "#0F0E0E",
+    color: "white",
   },
   title: {
     marginBottom: 20,
@@ -67,5 +87,12 @@ const styles = StyleSheet.create({
   clearButton: {
     color: "white",
     fontWeight: "700",
+  },
+  switch: {
+    margin: 20,
+  },
+  author: {
+    color: "gray",
+    fontSize: 12,
   },
 });
